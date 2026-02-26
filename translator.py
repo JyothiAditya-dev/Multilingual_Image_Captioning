a66fc165926206ee900b6bf6f6e528b7c7289439
JYOTHI ADITYA GIRI <adityajalli07@gmail.com>
2026-02-26 18:25:08 +0530

    Update translator.py

--- translator.py
+++ translator.py
@@ -4,27 +4,28 @@ print("Loading translation models... Please wait.")
 
 # Hindi translator
 translator_hi = pipeline(
-    task="translation",
+    task="translation_en_to_hi",
     model="Helsinki-NLP/opus-mt-en-hi"
 )
 
-# Multilingual translator (supports Telugu)
-translator_multi = pipeline(
-    task="translation",
+# Telugu translator using multilingual model
+translator_te = pipeline(
+    task="translation_en_to_te",
     model="Helsinki-NLP/opus-mt-en-mul"
 )
 
 print("Translation models loaded successfully.")
 
+
 def translate(text, language):
-    
+
     if language == "Hindi":
         result = translator_hi(text)
         return result[0]['translation_text']
-    
+
     elif language == "Telugu":
-        result = translator_multi(text, tgt_lang="te")
+        result = translator_te(text)
         return result[0]['translation_text']
-    
+
     else:
         return text
